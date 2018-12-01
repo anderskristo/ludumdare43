9c48847e61591d1f4ee82e4e09cf68f9dad50ef0
Pontus Bystedt <pontus.bystedt@adeprimo.se>
2018-12-01 14:32:58 +0100

    fugly endless scrolling of base tile :frowning:

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -15,6 +15,7 @@ export default class GameScene extends Phaser.Scene {
         this.coinLayer;
         this.text;
         this.score = 0;
+        this.moveSpeed = 20;
     }
 
     create() {
@@ -38,7 +39,7 @@ export default class GameScene extends Phaser.Scene {
         // set the boundaries of our game world
         this.physics.world.bounds.width = this.groundLayer.width;
         this.physics.world.bounds.height = this.groundLayer.height;
-
+        this.cursors = this.input.keyboard.createCursorKeys();
         this.createPlayer();
     }
 
@@ -56,6 +57,19 @@ export default class GameScene extends Phaser.Scene {
     }
 
     update(time, delta) {
+        if (this.cursors.right.isDown)
+        {
+            this.groundLayer.x = this.groundLayer.x - this.moveSpeed;
+            //var firstElems = this.groundLayer.culledTiles[0].layer.data[0].slice(0, 10);
+            //this.groundLayer.culledTiles[0].layer.data[0].push(firstElems);
 
+            if (this.groundLayer.x < -this.map.widthInPixels/2) {
+                this.groundLayer.x = 0;
+            }
+        }
+        else if (this.cursors.left.isDown)
+        {
+            this.groundLayer.x = this.groundLayer.x + this.moveSpeed;
+        }
     }
 }
