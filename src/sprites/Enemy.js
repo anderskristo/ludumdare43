48caa33278a74d730803cb61ecee86cb17ab074a
Pontus Bystedt <pontus.bystedt@adeprimo.se>
2018-12-01 15:32:02 +0100

    first enemy :mailbox_with_no_mail:

---
+++ src/sprites/Enemy.js
@@ -0,0 +1,11 @@
+export default class Enemy extends Phaser.GameObjects.Sprite {
+    constructor(Config) {
+        super(Config.scene, Config.x, Config.y, 'enemy');
+
+        //Config.scene.physics.world.enable(this)
+        this.scene = Config.scene;
+        //this.body.setBounce(0.8)
+
+        this.scene.add.existing(this);
+    }
+}
