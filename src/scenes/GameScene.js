aa4eca7e8e9726839c649b73e90abc7feff6c59f
Pontus Bystedt <pontus.bystedt@adeprimo.se>
2018-12-01 10:38:50 +0100

    Almost fixing sound stuff :sweat_smile:

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -6,12 +6,12 @@ export default class GameScene extends Phaser.Scene {
             key: 'GameScene'
         });
 
-        this.dogBarkingBuffer = null;
+        game.dogBarkingBuffer = null;
 
         // Fix up prefixing
         window.AudioContext = window.AudioContext || window.webkitAudioContext;
-        this.context = new AudioContext();
-        this.analyser = this.context.createAnalyser();
+        game.context = new AudioContext();
+        game.analyser = game.context.createAnalyser();
     }
 
     create() {
@@ -22,10 +22,10 @@ export default class GameScene extends Phaser.Scene {
     }
 
     update() {
-        this.analyser.fftSize = 2048;
-        var bufferLength = this.analyser.frequencyBinCount;
+        game.analyser.fftSize = 2048;
+        var bufferLength = game.analyser.frequencyBinCount;
         var dataArray = new Uint8Array(bufferLength);
-        this.analyser.getByteTimeDomainData(dataArray);
+        game.analyser.getByteTimeDomainData(dataArray);
 
         // Get a canvas defined with ID "oscilloscope"
         var canvas = document.getElementById("auCanvas");
@@ -36,7 +36,7 @@ export default class GameScene extends Phaser.Scene {
 
         //requestAnimationFrame(draw);
 
-        this.analyser.getByteTimeDomainData(dataArray);
+        game.analyser.getByteTimeDomainData(dataArray);
 
         canvasCtx.fillStyle = "rgb(200, 200, 200)";
         canvasCtx.fillRect(0, 0, canvas.width, canvas.height);
@@ -47,6 +47,7 @@ export default class GameScene extends Phaser.Scene {
         canvasCtx.beginPath();
 
         var sliceWidth = canvas.width * 1.0 / bufferLength;
+
         var x = 0;
 
         for (var i = 0; i < bufferLength; i++) {
@@ -62,7 +63,7 @@ export default class GameScene extends Phaser.Scene {
 
             x += sliceWidth;
         }
-        canvasCtx.lineTo(20, 20);
+        canvasCtx.lineTo(25, 75);
         canvasCtx.lineTo(canvas.width, canvas.height / 2);
         canvasCtx.stroke();
     }
@@ -71,7 +72,7 @@ export default class GameScene extends Phaser.Scene {
 
         requestAnimationFrame(draw);
 
-        this.analyser.getByteTimeDomainData(dataArray);
+        game.analyser.getByteTimeDomainData(dataArray);
 
         canvasCtx.fillStyle = "rgb(200, 200, 200)";
         canvasCtx.fillRect(0, 0, canvas.width, canvas.height);
@@ -109,11 +110,11 @@ export default class GameScene extends Phaser.Scene {
 
         // Decode asynchronously
         request.onload = function () {
-            console.log(this.context)
-            this.context.decodeAudioData(request.response, function (buffer) {
+            console.log(game.context)
+            game.context.decodeAudioData(request.response, function (buffer) {
                 console.log('OK', buffer);
-                this.dogBarkingBuffer = buffer;
-                playSound(buffer);
+                game.dogBarkingBuffer = buffer;
+                this.playSound(buffer);
             }, function (error) {
                 console.error('ERR', error);
             });
@@ -122,11 +123,140 @@ export default class GameScene extends Phaser.Scene {
     }
 
     playSound(buffer) {
-        var source = context.createBufferSource(); // creates a sound source
+        var source = game.context.createBufferSource(); // creates a sound source
         source.buffer = buffer; // tell the source which sound to play
         source.connect(context.destination); // connect the source to the context's destination (the speakers)
         source.start(0); // play the source now
         // note: on older systems, may have to use deprecated noteOn(time);
     }
+}
+/*
+//const game = new Phaser.Game(config);
+console.log('audioCtx');
+//800, 600, Phaser.CANVAS, 'phaser-example', { preload: preload, create: create, update: update, render: render }
+var game = new Phaser.Game(config);
+console.log(game)
+var mods = [];
+var current = 0;
+
+var vumeter = [];
+var channels = [];
+
+var module;
+
+function preload() {
+
+    this.load.script('protracker', 'assets/ProTracker.js');
+
+    this.load.image('vu', 'assets/logo.png');
+    this.load.image('logo', 'assets/logo.png');
+    this.load.image('bg', 'assets/logo.png');
+    this.load.image('vulkaiser', 'assets/logo.png');
+
+    this.load.binary('shampoo', 'assets/act_of_impulse.mod', modLoaded, this);
+}
+
+function modLoaded(key, data) {
+
+    mods.push(key);
+    var buffer = new Uint8Array(data);
+    return buffer;
 
 }
+
+function load_next_module()
+{
+    current == mods.length - 1 ? current = 0 : current++;
+
+    module.stop();
+    module.clearsong();
+
+    //module.buffer = game.cache.getBinary(mods[current]);
+    module.buffer = game.cache.binary.get(mods[current]);
+    module.parse();
+
+    // BUG if width==0
+    // for (var i = 0; i < vumeter.length; i++)
+    // {
+    //     vumeter[i].width = 1;
+    // }
+
+}
+
+function create() {
+
+    this.add.sprite(0, 0, 'bg');
+    this.add.sprite(500, 32, 'logo');
+    this.add.sprite(580, 371, 'vulkaiser');
+
+    for (var i = 0, y = 200; i < 4; i++, y += 50)
+    {
+        vumeter[i] = this.add.sprite(400, y, 'vu');
+//vumeter[i].crop(new Phaser.Geom.Rectangle(0, 0, 300, 30));
+        vumeter[i].frame.cutHeight = 30;
+        vumeter[i].frame.cutWidth = 300;
+    }
+
+    module = new Protracker();
+
+    //module.play() has to be called from a callback
+    module.onReady = function() {
+        module.play();
+    };
+console.log(module)
+    //this.cache = new BaseCache();
+    //this.cache.add('bacon', mods[current]);
+    //module.buffer = this.cache.get(bacon);
+    module.buffer = game.cache.binary.get(mods[current]);
+    module.parse();
+
+    //this.input.onDown.add(load_next_module, this);
+    this.input.on('down',load_next_module);
+
+}
+
+function update() {
+
+    //  module.sample = array of Objects containing informations about a played sample
+
+    for (var i = 0; i < vumeter.length; i++)
+    {
+        if (module.channel[i])
+        {
+            var smp_index = module.channel[i].sample;
+            channels[i] = { sample_index:smp_index, sample_name: module.sample[smp_index].name };
+
+            var w = Math.round(module.vu[i] * 1200);
+
+            if (w > 300)
+            {
+                w = 300;
+            }
+
+            vumeter[i].cropRect.width = w;
+            vumeter[i].updateCrop();
+        }
+    }
+
+}
+
+function render() {
+
+    for (var i = 0, y = 32; i < vumeter.length; i++, y += 32)
+    {
+        if (channels[i])
+        {
+            this.debug.text('Channel #' + i + ' : sample ' + channels[i].sample_index + '  ' + channels[i].sample_name, 16, y);
+            // game.debug.text('vu' + i + ': ' + module.vu[i], 16, 350 + y);
+        }
+    }
+
+    this.debug.text('Position: ' + module.position, 16, 160);
+    this.debug.text('Pattern: ' + module.row, 16, 192);
+    this.debug.text('BPM: ' + module.bpm, 16, 224);
+    this.debug.text('Speed: ' + module.speed, 16, 256);
+    this.debug.text('Name: ' + module.title, 16, 288);
+    this.debug.text('Signature: ' + module.signature, 16, 320);
+
+}
+*/
