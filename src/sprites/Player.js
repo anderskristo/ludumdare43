933c978ed0e21ce884644e5716482ed5567d60b7
anderskristo <anderskristo@gmail.com>
2018-12-01 14:36:18 +0100

    le player is updated :smirk:

--- src/sprites/Player.js
+++ src/sprites/Player.js
@@ -5,11 +5,26 @@ export default class Player extends Phaser.GameObjects.Sprite {
         Config.scene.physics.world.enable(this)
         this.scene = Config.scene;
         this.body.setBounce(0.2)
+        //this.setColliderWorldBounds(true)
         this.input = this.scene.input.keyboard.createCursorKeys();
-
 
         this.scene.input.keyboard.on('keydown_Q', function () {
             console.log('pressed Q');
+
+            this.body.setVelocityY(500);
         }, this);
+
+        this.scene.add.existing(this);
+    }
+
+    update(time, delta) {
+        console.log('#fff');
+        if (this.input.up.isDown) {
+            console.log('jump');
+            this.body.setVelocityY(-64);
+        } else if (this.input.down.isDown) {
+            console.log('ducka för fan');
+            this.body.setVelocityY(64);
+        }
     }
 }
