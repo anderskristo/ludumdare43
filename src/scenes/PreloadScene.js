8d1b0007b1d29d94f697028e68c3d875591046b4
theBigBadBacon <pontus.bystedt@adeprimo.se>
2018-12-02 17:09:52 +0100

    Fix progressbar 4 realz :spock-hand:

--- src/scenes/PreloadScene.js
+++ src/scenes/PreloadScene.js
@@ -22,16 +22,7 @@ export default class PreloadScene extends Phaser.Scene {
                 fill: '#ffffff'
             }
         });
-        this.percentText = this.make.text({
-            x: width / 2,
-            y: height / 2 - 5,
-            text: '0%',
-            style: {
-                font: '18px monospace',
-                fill: '#ffffff'
-            }
-        });
-
+        
         this.assetText = this.make.text({
             x: width / 2,
             y: height / 2 + 50,
@@ -43,33 +34,41 @@ export default class PreloadScene extends Phaser.Scene {
         });
         this.assetText.setOrigin(0.5, 0.5);
         this.loadingText.setOrigin(0.5, 0.5);
-        this.percentText.setOrigin(0.5, 0.5);
+        
 
         // Create background and prepare the loading bar
         this.cameras.main.setBackgroundColor(0x0c0b0b);
         this.fullBar = this.add.graphics();
-        this.fullBar.fillStyle(0xffffff, 1);
+        this.fullBar.fillStyle(0x333333, 1);
         this.fullBar.fillRect((width / 4) - 2, (height / 2), (width / 2), 20);
         this.progress = this.add.graphics();
+
+        this.percentText = this.make.text({
+            x: width / 2,
+            y: height / 2 + 10,
+            text: '0%',
+            style: {
+                font: '18px monospace',
+                fill: '#fff'
+            }
+        });
+        this.percentText.setOrigin(0.5, 0.5);
 
         // Pass loading progress as value
         this.load.on('progress', function (value) {
             this.percentText.setText(parseInt(value * 100) + '%');
             this.progress.clear();
-            this.progress.fillStyle(0x00ff80, 1);
-            this.progress.fillRect((this.cameras.main.width / 4), (height / 2), (width / 2), 20);
+            this.progress.fillStyle(0x00aa80, value);
+            this.progress.fillRect((width / 4), (height / 2), (width / 2), 20);
         }, this);
 
         // Pass loading progress as value
         this.load.on('fileprogress', function (file) {
             this.assetText.setText('Loading asset: ' + file.key);
-            this.progress.clear();
-            this.progress.fillStyle(0x00ff80, 1);
-            this.progress.fillRect((this.cameras.main.width / 4), (height / 2), (width / 2), 20);
         }, this);
 
         // Cleanup after loading is done
-        this.load.on('complete', function () {
+        this.load.on('completea', function () {
             this.progress.destroy();
             this.percentText.destroy();
             this.assetText.destroy();
@@ -80,7 +79,7 @@ export default class PreloadScene extends Phaser.Scene {
     }
 
     create() {
-        this.scene.add('IntroScene', IntroScene);
-        this.scene.start('IntroScene');
+        //this.scene.add('IntroScene', IntroScene);
+        //this.scene.start('IntroScene');
     }
 }
